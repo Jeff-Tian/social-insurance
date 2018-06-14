b60fd78e103a4a01dba55d53274c0207c034bbf1
Jefåf Tian <jeff.tian@outlook.com>
2018-06-14 23:54:45 +0800

    UI and print function

--- ReadYBCard/ReadYBCard/Form1.Designer.cs
+++ ReadYBCard/ReadYBCard/Form1.Designer.cs
@@ -29,32 +29,31 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.btnReadCard = new System.Windows.Forms.Button();
             this.dgv = new System.Windows.Forms.DataGridView();
-            this.Print = new System.Windows.Forms.DataGridViewButtonColumn();
             this.KH = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.BRXM = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.JYSJ = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.XDT = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.jLEISDB2DataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.jLEISDB2DataSet = new ReadYBCard.JLEISDB2DataSet();
             this.axJLPrintECG1 = new AxJLPrintECGLib.AxJLPrintECG();
             this.button1 = new System.Windows.Forms.Button();
-            this.lblInfo = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.jLEISDB2DataSetBindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.jLEISDB2DataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.axJLPrintECG1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnReadCard
             // 
-            this.btnReadCard.Font = new System.Drawing.Font("微软雅黑", 14.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.btnReadCard.Location = new System.Drawing.Point(17, 22);
+            this.btnReadCard.Font = new System.Drawing.Font("微软雅黑", 42F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnReadCard.Location = new System.Drawing.Point(373, 112);
             this.btnReadCard.Margin = new System.Windows.Forms.Padding(8);
             this.btnReadCard.Name = "btnReadCard";
-            this.btnReadCard.Size = new System.Drawing.Size(452, 130);
+            this.btnReadCard.Size = new System.Drawing.Size(996, 245);
             this.btnReadCard.TabIndex = 0;
             this.btnReadCard.Text = "查找心电报告";
             this.btnReadCard.UseVisualStyleBackColor = true;
@@ -67,11 +66,9 @@
             this.dgv.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Print,
             this.KH,
             this.BRXM,
-            this.JYSJ,
-            this.XDT});
+            this.JYSJ});
             this.dgv.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.dgv.Location = new System.Drawing.Point(0, 834);
             this.dgv.Name = "dgv";
@@ -79,18 +76,14 @@
             this.dgv.RowTemplate.Height = 44;
             this.dgv.Size = new System.Drawing.Size(1729, 685);
             this.dgv.TabIndex = 3;
+            this.dgv.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_CellClick);
             this.dgv.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_CellContentClick);
-            // 
-            // Print
-            // 
-            this.Print.HeaderText = "打印";
-            this.Print.Name = "Print";
-            this.Print.ReadOnly = true;
-            this.Print.UseColumnTextForButtonValue = true;
             // 
             // KH
             // 
             this.KH.DataPropertyName = "KH";
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("微软雅黑", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.KH.DefaultCellStyle = dataGridViewCellStyle4;
             this.KH.HeaderText = "卡号";
             this.KH.Name = "KH";
             this.KH.ReadOnly = true;
@@ -98,6 +91,8 @@
             // BRXM
             // 
             this.BRXM.DataPropertyName = "BRXM";
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("微软雅黑", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.BRXM.DefaultCellStyle = dataGridViewCellStyle5;
             this.BRXM.HeaderText = "姓名";
             this.BRXM.Name = "BRXM";
             this.BRXM.ReadOnly = true;
@@ -105,31 +100,16 @@
             // JYSJ
             // 
             this.JYSJ.DataPropertyName = "JYSJ";
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("微软雅黑", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.JYSJ.DefaultCellStyle = dataGridViewCellStyle6;
             this.JYSJ.HeaderText = "检查时间";
             this.JYSJ.Name = "JYSJ";
             this.JYSJ.ReadOnly = true;
             // 
-            // XDT
-            // 
-            this.XDT.DataPropertyName = "XDT";
-            this.XDT.HeaderText = "心电图";
-            this.XDT.Name = "XDT";
-            this.XDT.ReadOnly = true;
-            // 
-            // jLEISDB2DataSetBindingSource
-            // 
-            this.jLEISDB2DataSetBindingSource.DataSource = this.jLEISDB2DataSet;
-            this.jLEISDB2DataSetBindingSource.Position = 0;
-            // 
-            // jLEISDB2DataSet
-            // 
-            this.jLEISDB2DataSet.DataSetName = "JLEISDB2DataSet";
-            this.jLEISDB2DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
             // axJLPrintECG1
             // 
             this.axJLPrintECG1.Enabled = true;
-            this.axJLPrintECG1.Location = new System.Drawing.Point(1410, 38);
+            this.axJLPrintECG1.Location = new System.Drawing.Point(1649, 396);
             this.axJLPrintECG1.Name = "axJLPrintECG1";
             this.axJLPrintECG1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axJLPrintECG1.OcxState")));
             this.axJLPrintECG1.Size = new System.Drawing.Size(68, 76);
@@ -138,29 +118,32 @@
             // button1
             // 
             this.button1.Enabled = false;
-            this.button1.Font = new System.Drawing.Font("微软雅黑", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button1.Location = new System.Drawing.Point(526, 22);
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 42F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(373, 411);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(506, 130);
+            this.button1.Size = new System.Drawing.Size(996, 231);
             this.button1.TabIndex = 5;
             this.button1.Text = "打印";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // lblInfo
+            // button2
             // 
-            this.lblInfo.AutoSize = true;
-            this.lblInfo.Location = new System.Drawing.Point(51, 191);
-            this.lblInfo.Name = "lblInfo";
-            this.lblInfo.Size = new System.Drawing.Size(0, 30);
-            this.lblInfo.TabIndex = 6;
+            this.button2.Font = new System.Drawing.Font("微软雅黑", 42F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(373, 693);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(996, 231);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "退出";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 30F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1729, 1519);
-            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.axJLPrintECG1);
             this.Controls.Add(this.dgv);
@@ -170,12 +153,11 @@
             this.Name = "Form1";
             this.Text = "报告打印";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.jLEISDB2DataSetBindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.jLEISDB2DataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.axJLPrintECG1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -184,15 +166,13 @@
         private System.Windows.Forms.Button btnReadCard;
         private System.Windows.Forms.DataGridView dgv;
         private System.Windows.Forms.BindingSource jLEISDB2DataSetBindingSource;
-        private JLEISDB2DataSet jLEISDB2DataSet;
+        
         private AxJLPrintECGLib.AxJLPrintECG axJLPrintECG1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.DataGridViewButtonColumn Print;
         private System.Windows.Forms.DataGridViewTextBoxColumn KH;
         private System.Windows.Forms.DataGridViewTextBoxColumn BRXM;
         private System.Windows.Forms.DataGridViewTextBoxColumn JYSJ;
-        private System.Windows.Forms.DataGridViewTextBoxColumn XDT;
-        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- ReadYBCard/ReadYBCard/Form1.cs
+++ ReadYBCard/ReadYBCard/Form1.cs
@@ -44,16 +44,19 @@ namespace ReadYBCard
         {
 
             YBLib.CardInfo info = YBLib.GetCardInfo;
-            lblInfo.Text = "卡信息:" + info.CardInfoString();
+            //lblInfo.Text = "卡信息:" + info.CardInfoString();
             Console.WriteLine(info.CardInfoString());
 
-            //QueryReport(info.CardNo);
-            QueryReport("F02545847");
+            QueryReport(info.CardNo);
+            //QueryReport("K04185546");
         }
 
         public void QueryReport(String cardNumber)
         {
-            SqlConnection conn = new SqlConnection("Data Source=192.168.0.247\\SQL2005;Initial Catalog=JLEISDB2;Persist Security Info=True;User ID=jlxdt;Password=jlxdt");
+            //SqlConnection conn = new SqlConnection("Data Source=192.168.0.247\\SQL2005;Initial Catalog=JLEISDB2;Persist Security Info=True;User ID=jlxdt;Password=jlxdt");
+            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["main"].ConnectionString;
+            
+            SqlConnection conn = new SqlConnection(connectionString);
 
             try
             {
@@ -72,6 +75,8 @@ namespace ReadYBCard
                         dgv.AutoGenerateColumns = false;
                         dgv.DataSource = dt.AsDataView();
                         button1.Enabled = true;
+
+                        this.dgv_CellClick(null, new DataGridViewCellEventArgs(0, 0));
                     }
                     else
                     {
@@ -93,20 +98,66 @@ namespace ReadYBCard
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //axJLPrintECG1.setPrintTemplateUrl(@"C:\Users\Jeff\Documents\WeChat Files\jie-tian\Files\JLCtrls\print.ini");
+            string iniPath = System.Configuration.ConfigurationManager.AppSettings["iniPath"];
+            iniPath = System.IO.Path.GetFullPath("./print.ini");
+                        
+            int res = axJLPrintECG1.setPrintTemplateUrl(iniPath);
+            if(res != 0)
+            {
+                MessageBox.Show("设置 ini 发生错误： " + res.ToString());
+            }
+            
 
-            axJLPrintECG1.printEcgPaper(0);
+            int returnValue = axJLPrintECG1.printEcgPaper(0);
+
+            if (returnValue != 0)
+            {
+                switch (returnValue) { 
+                    case 6008:
+                        MessageBox.Show("打印发生其他错误，请稍后再试。");
+                        break;
+                    default:
+                        MessageBox.Show("打印发生未知错误，请稍后再试。");
+                        break;
+                }
+            }
         }
 
         private void dgv_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+        }
+
+        private void dgv_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
             DataRow row = dt.Rows[e.RowIndex];
+
+
+            DateTime birthday = (DateTime)row["birthday"];
+            DateTime now = DateTime.Now;
+            TimeSpan diff = now.Subtract(birthday);
+
             axJLPrintECG1.setEcgBinData(row["XDT"]);
             axJLPrintECG1.setEcgData(row["XDT"].ToString());
             axJLPrintECG1.setComment(row["ZD"].ToString(), row["BGRXM"].ToString());
-            //axJLPrintECG1.setMeasureBinData(row["XDT"]);
-            //axJLPrintECG1.setMeasureData(row["XDT"].ToString());
-            axJLPrintECG1.setPatientInfo(row["KH"].ToString(), row["BRXM"].ToString(), row["BRXB"].ToString().Equals("1") ? "男" : "女", "", (int)row["age"], "");
+            axJLPrintECG1.setMeasureBinData(row["measuredata"]);
+            axJLPrintECG1.setMeasureData(row["measuredata"].ToString());
+            axJLPrintECG1.setPatientInfo(row["KH"].ToString(), row["BRXM"].ToString(), row["BRXB"].ToString().Equals("1") ? "男" : "女", "", (int)(diff.Days / 365), "");
+
+
+            //this.labelSelectedInfo.Text = "选中了第 " + (e.RowIndex + 1).ToString() + " 行。";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.btnReadCard.Left = (this.Width - this.btnReadCard.Width) / 2;
+            this.button1.Left = (this.Width - this.button1.Width) / 2;
+            this.button2.Left = (this.Width - this.button2.Width) / 2;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.dgv.DataSource = new DataTable();
+            this.button1.Enabled = false;
         }
     }
 }
